1bd570ade3db59c81ba3db4c3d8a0baa398b6a53
花木凋零成兰 <18570354653@163.com>
2025-03-24 21:06:53 +0800

    测试-客户端Socket

---
+++ test-rpc-client/src/main/java/top/ytazwc/test/SocketClient.java
@@ -0,0 +1,34 @@
+package top.ytazwc.test;
+
+import top.ytazwc.rpc.config.RpcServiceConfig;
+import top.ytazwc.rpc.transport.proxy.RpcClientProxy;
+import top.ytazwc.rpc.transport.socket.RpcClientSocket;
+import top.ytazwc.test.service.UserService;
+
+/**
+ * @author 花木凋零成兰
+ * @title SocketClient
+ * @date 2025-03-24 20:00
+ * @package top.ytazwc.test
+ * @description
+ */
+public class SocketClient {
+
+    public static void main(String[] args) {
+
+        RpcServiceConfig config = new RpcServiceConfig();
+        config.setVersion("1.0.0");
+        config.setGroup("service");
+        RpcClientProxy proxy = new RpcClientProxy(new RpcClientSocket(), config);
+        UserService service = proxy.getProxy(UserService.class);
+
+        String user = service.getUser();
+        System.out.println(user);
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+    }
+
+}
